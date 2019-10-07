c5fc887b56923fe65a80b1323dfd604d0650440c
Tristan BONNESON <trbonnes@e2r11p20.42.fr>
2019-10-07 14:51:28 +0200

    ca marche en fait

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 14:33:10 by trbonnes          #+#    #+#             */
-/*   Updated: 2019/10/07 14:39:11 by trbonnes         ###   ########.fr       */
+/*   Updated: 2019/10/07 14:50:52 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_bzero(void *s, unsigned int n)
 	ptr = s;
 	while (n > 0)
 	{
-		*ptr++ = '\0';
+		*ptr++ = 0;
 		n--;
 	}
 }
