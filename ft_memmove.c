9f25cd20315f82780469cea4cfdeeaa8ec2abb92
Tristan BONNESON <trbonnes@e2r11p20.42.fr>
2019-10-07 15:46:26 +0200

    jai compris

---
+++ ft_memmove.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 15:19:20 by trbonnes          #+#    #+#             */
+/*   Updated: 2019/10/07 15:45:29 by trbonnes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dest, const void *src, unsigned int n)
+{
+	unsigned char	*ptrd;
+	const char		*ptrs;
+
+	ptrd = dest;
+	ptrs = src;
+	if 
+	return (dest);
+}
+
+/* ordre debut dest et debut src pout le chevauchement */
