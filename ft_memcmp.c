bc1982313ca72db5ddfb0e1718c3c6f933b25e96
Tristan BONNESON <trbonnes@e1r3p14.42.fr>
2019-10-08 09:50:22 +0200

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/08 09:37:50 by trbonnes          #+#    #+#             */
+/*   Updated: 2019/10/08 09:50:10 by trbonnes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_memcmp(const void *s1, const void *s2, unsigned int n)
+{
+	const unsigned char *ptr1;
+	const unsigned char *ptr2;
+
+	ptr1 = s1;
+	ptr2 = s2;
+	if (ptr1 == ptr2 || n == 0)
+		return (0);
+	while (n > 0)
+	{
+		if (*ptr1 != *ptr2)
+			return (*ptr1 - *ptr2);
+		ptr1++;
+		ptr2++;
+		n--;
+	}
+	return (0);
+}
