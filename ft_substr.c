ca6fa8eeb401515fcae4461ee3de75c52c308981
Tristan BONNESON <trbonnes@e1r3p14.42.fr>
2019-10-08 17:50:28 +0200

    bug substr OK

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 15:43:13 by trbonnes          #+#    #+#             */
-/*   Updated: 2019/10/08 17:00:55 by trbonnes         ###   ########.fr       */
+/*   Updated: 2019/10/08 17:48:51 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char *dest;
 	char *ptr;
+	char *src;
 
 	if ((dest = malloc(len)) == NULL)
 		return (NULL);
 	dest[len] = '\0';
 	ptr = dest;
-	s = s + start;
-	while (*s != '\0' && *dest != '\0')
+	src = (char *)s + start;
+	while (*src != '\0' && dest != (ptr + len))
 	{
-		*dest = *s;
+		*dest = *src;
 		dest++;
-		s++;
+		src++;
 	}
+	*dest = '\0';
 	return (ptr);
 }
