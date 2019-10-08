a816441db0cf7b67478ac43dbe62736c4f74ab3f
Tristan BONNESON <trbonnes@e1r3p14.42.fr>
2019-10-08 09:07:04 +0200

    pre increment

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 08:23:10 by trbonnes          #+#    #+#             */
-/*   Updated: 2019/10/08 08:57:45 by trbonnes         ###   ########.fr       */
+/*   Updated: 2019/10/08 09:05:53 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	*ft_memccpy(void *dest, const void *src, int c, unsigned int n)
 	if (*ptrs == c)
 	{
 		*ptrd = *ptrs;
-		return (ptrd++);
+		return (++ptrd);
 	}
 	else
 		return (0);
