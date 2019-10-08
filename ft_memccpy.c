cb8adfc7818fadf51c50785cfd83985bbd3ea769
Tristan BONNESON <trbonnes@e1r3p14.42.fr>
2019-10-08 09:00:10 +0200

    bettre

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 08:23:10 by trbonnes          #+#    #+#             */
-/*   Updated: 2019/10/08 08:39:26 by trbonnes         ###   ########.fr       */
+/*   Updated: 2019/10/08 08:57:45 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,11 @@ void	*ft_memccpy(void *dest, const void *src, int c, unsigned int n)
 		ptrs++;
 		n--;
 	}
-	return (dest);
+	if (*ptrs == c)
+	{
+		*ptrd = *ptrs;
+		return (ptrd++);
+	}
+	else
+		return (0);
 }
