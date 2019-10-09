5bdd99c6f0d6dc694df0cbd1bceabdce4fe3d8a0
Tristan BONNESON <trbonnes@e1r3p14.42.fr>
2019-10-09 08:49:10 +0200

    Git lab

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,43 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 16:13:25 by trbonnes          #+#    #+#             */
-/*   Updated: 2019/10/08 16:27:44 by trbonnes         ###   ########.fr       */
+/*   Updated: 2019/10/09 08:38:53 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strjoin(char const *s1, char const *s2)
+size_t		ft_strlen(const char *s)
 {
-	
+	size_t i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	char *dest;
+	char *ptr;
+
+	dest = (malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1)));
+	if (dest == NULL)
+		return (NULL);
+	ptr = dest;
+	while (*s1 != '\0')
+	{
+		*dest = *s1;
+		dest++;
+		s1++;
+	}
+	while (*s2 != '\0')
+	{
+		*dest = *s2;
+		dest++;
+		s2++;
+	}
+	*dest = '\0';
+	return (ptr);
 }
