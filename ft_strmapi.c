b02bcf485ec1513e2f86524eb6cc128de417ce63
Tristan BONNESON <trbonnes@e1r3p14.42.fr>
2019-10-09 15:48:48 +0200

    fin codage fonctions de base

---
+++ ft_strmapi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/09 14:37:09 by trbonnes          #+#    #+#             */
+/*   Updated: 2019/10/09 14:52:58 by trbonnes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+size_t		ft_strlen(const char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char				*str;
+	unsigned int		i;
+
+	if (!(str = malloc(sizeof(char) * ft_strlen(s) + 1)))
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	return (str);
+}
