66e3c1df58e11562e6a6ce578f89f894f5aa046a
Tristan BONNESON <trbonnes@e1r3p14.42.fr>
2019-10-10 19:11:53 +0200

    strlcpy de tes morts

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,36 +6,28 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 13:52:41 by trbonnes          #+#    #+#             */
-/*   Updated: 2019/10/10 10:57:33 by trbonnes         ###   ########.fr       */
+/*   Updated: 2019/10/10 19:03:31 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-static int	ft_strlen(const char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
 size_t		ft_strlcpy(char *dest, const char *src, size_t n)
 {
 	unsigned int i;
+	unsigned int count;
 
 	i = 0;
+	count = 0;
+	while (src[count] != '\0')
+		count++;
+	if (n == 0)
+		return (count);
 	while (i < n - 1 && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	if (n == 0)
-		dest[0] = '\0';
-	return (ft_strlen(src));
+	return (count);
 }
